8ac45fd0f2107bc78d20bfd7e67b9eb8293d55fd
haseeb235 <f190926@cfd.nu.edu.pk>
2023-12-06 15:48:50 +0500

    My Project

---
+++ Desktop/obstacle_detection/android/app/src/main/kotlin/com/example/obstacle_detection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.obstacle_detection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Desktop/obstacle_detection/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:obstacle_detection/HomePage.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription> cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Obstacle Detection',
+      home: homeScreen(),
+    );
+  }
+}
